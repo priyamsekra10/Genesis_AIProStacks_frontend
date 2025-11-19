214ebb4e26cba9c90f8e3f08dd9d4f2ace7f0850
Priyam Sekra <priyam.sekra@novusvistatech.com>
2025-11-19 12:18:24 +0400

    error solving

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,23 @@
 // https://stackoverflow.com/questions/63124432/how-do-i-configure-mini-css-extract-plugin-in-gatsby
 exports.onCreateWebpackConfig = (helper) => {
   const { stage, actions, getConfig } = helper;
+  
+  // Add webpack fallbacks for Node.js modules
+  actions.setWebpackConfig({
+    resolve: {
+      fallback: {
+        http: false,
+        https: false,
+        zlib: false,
+        stream: false,
+        util: false,
+        buffer: false,
+        assert: false,
+        url: false,
+      },
+    },
+  });
+
   if (stage === 'develop' || stage === 'build-javascript') {
     const config = getConfig();
     const miniCssExtractPlugin = config.plugins.find(
